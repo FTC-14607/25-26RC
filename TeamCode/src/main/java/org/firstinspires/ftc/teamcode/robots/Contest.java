f3ecb30e16c3a895fa7b0636097cb20a69d314c2
cduckling <abhirajmallangi@outlook.com>
2025-11-28 10:55:33 -0500

    more stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/Contest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/Contest.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.robots;
 
-public class Contest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Contest extends MecanumDrive {
+    public DcMotorEx a, b, c, d, e, f;
+
+    // a
+    // b
+    // c
+    // d
+    // e
+    // f
+
+    public Contest(LinearOpMode opmode) {
+        super(opmode);
+
+        a  = hardwareMap.get(DcMotorEx.class, "a");
+        b  = hardwareMap.get(DcMotorEx.class, "b");
+        c  = hardwareMap.get(DcMotorEx.class, "c");
+        d  = hardwareMap.get(DcMotorEx.class, "d");
+        e  = hardwareMap.get(DcMotorEx.class, "e");
+        f  = hardwareMap.get(DcMotorEx.class, "f");
+
+
+    }
 }
