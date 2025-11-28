016cfeb94e4085586325ea07ed316ece039ff6ec
cduckling <abhirajmallangi@outlook.com>
2025-11-28 11:23:00 -0500

    Abhiraj Mallangi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/Contest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/Contest.java
@@ -2,26 +2,17 @@ package org.firstinspires.ftc.teamcode.robots;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Contest extends MecanumDrive {
-    public DcMotorEx a, b, c, d, e, f;
+    public Servo a, b;
+    public DcMotorEx c, d;
 
-    // a
-    // b
-    // c
-    // d
-    // e
-    // f
+    // a, b: intake servos
+    // c, d: flywheel motors
 
     public Contest(LinearOpMode opmode) {
         super(opmode);
-
-        a  = hardwareMap.get(DcMotorEx.class, "a");
-        b  = hardwareMap.get(DcMotorEx.class, "b");
-        c  = hardwareMap.get(DcMotorEx.class, "c");
-        d  = hardwareMap.get(DcMotorEx.class, "d");
-        e  = hardwareMap.get(DcMotorEx.class, "e");
-        f  = hardwareMap.get(DcMotorEx.class, "f");
 
 
     }
